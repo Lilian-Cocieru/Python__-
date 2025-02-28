296ab5f40d47d8cc3afe8d1667ee32df05701b50
Lilian-Cocieru <lilianvideo.lc@gmail.com>
2025-02-28 11:35:19 +0300

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,7 @@
+def info(name, age):
+    print(f"Привет, {name}! Тебе {age} лет, ты будущий Backend-разработчик!")
+
+name = input("Как тебя зовут? ")
+age = int(input("Сколько тебе лет? "))
+
+info(name, age)
